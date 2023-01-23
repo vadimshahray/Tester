22497747f37605d0a916734bad2dc751f91e4e0f
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-24 03:05:57 +0700

    feat: configure project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@selectors': './src/selectors',
+          '@slices': './src/slices',
+          '@storage': './src/storage',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+}
